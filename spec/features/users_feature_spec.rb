f00ad7a3cccce56926d1f8e7eed3a1365608120f
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-06-07 14:39:45 +0100

    set up intial devise plus two tests

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,13 @@
+feature "User can sign in and out" do
+  context "user not signed in and on the homepage" do
+    it "should see a 'sign in' link and a 'sign up' link" do
+      visit '/'
+      expect(page).to have_link 'Sign in'
+      expect(page).to have_link 'Sign up'
+    end
+    it "should not see 'sign out' link" do
+      visit'/'
+      expect(page).not_to have_link 'Sign out'
+    end
+  end
+end
