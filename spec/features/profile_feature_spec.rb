75297b8648afc8fc34967ac94937b25b723cd7b3
Noah Pollock <pollocknoah@gmail.com>
2016-06-08 10:25:10 +0100

    IOUs displaying on profile page

--- spec/features/profile_feature_spec.rb
+++ spec/features/profile_feature_spec.rb
@@ -1,16 +1,24 @@
 feature 'profile' do
-  context 'page has several pending uois' do
-    before do
-      Iou.create title: "Pizza party", amount: 20, name: "Noah", contact: "07123456789"
-      Iou.create title: "Cocktails!", amount: 50, name: "Ferg", contact: "07987654321"
-      Iou.create title: "Cables", amount: 200, name: "Lucy", contact: "07223344556"
-    end
+  before do
+    sign_up
+  end
 
-    scenario "UOIs show on profile page as pending" do
-      within "#pending_uois" do
+  context 'page shows pending uois' do
+    scenario 'UOIs show on profile page as pending' do
+      create_iou
+      within 'div#pending-uois' do
         list = find('ul').all('li').collect(&:text)
-        expect(list.count).to eq 3
+        expect(list.count).to eq 1
       end
     end
   end
+
+  context 'no ious' do
+    scenario 'show message when no ious' do
+      within 'div#pending-uois' do
+        expect(page).to have_content('NO IOUS!!!')
+      end
+    end
+  end
+
 end
