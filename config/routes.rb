77cdeb7783ef65e3f5d9dd13044090ac0097942d
Noah Pollock <pollocknoah@gmail.com>
2016-06-09 18:54:54 +0100

    Delete button added, slight refactor for colour

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   root to: "profile#index"
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
